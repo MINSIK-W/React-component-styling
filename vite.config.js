6290259cf269c157bb9e7497215851c2e219d905
ms <minsik90@icloud.com>
2024-07-28 18:06:20 +0900

    :wrench: tailwindCSS setting add

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,3 +6,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+*/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+});
